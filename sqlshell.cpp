747bcd02e00edc1b9b61ac1bd1121f8127c78bda
huidarin <darin.hui@outlook.com>
2022-04-06 16:48:39 -0700

    Updated skeleton with user loop and added to-dos

--- sqlshell.cpp
+++ sqlshell.cpp
@@ -3,53 +3,33 @@
 //sqlshell.cpp
 //4-6-2022
 
-//skeleton from example.cpp - used as starting point
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <iostream>
 #include "db_cxx.h"
 
-const std::string EXIT = "quit";
+using namespace std;
 
-// CREATE A DIRECTORY IN YOUR HOME DIR ~/cpsc5300/data before running this
-const char *HOME = "cpsc5300/data";
-const char *EXAMPLE = "example.db";
-const unsigned int BLOCK_SZ = 4096;
+const string EXIT = "quit";
 
-int main(void) {
-	std::cout << "Have you created a dir: ~/" << HOME  << "? (y/n) " << std::endl;
-	std::string ans;
-	std::cin >> ans;
-	if( ans[0] != 'y')
-		return 1;
-	const char *home = std::getenv("HOME");
-	std::string envdir = std::string(home) + "/" + HOME;
 
-	DbEnv env(0U);
-	env.set_message_stream(&std::cout);
-	env.set_error_stream(&std::cerr);
-	env.open(envdir.c_str(), DB_CREATE | DB_INIT_MPOOL, 0);
+//TO DO: add arguments in main func
+int main() {
+	//TO DO: create/open Berkeley DB env (probably in different file)
+	
 
-	Db db(&env, 0);
-	db.set_message_stream(env.get_message_stream());
-	db.set_error_stream(env.get_error_stream());
-	db.set_re_len(BLOCK_SZ); // Set record length to 4K
-	db.open(NULL, EXAMPLE, NULL, DB_RECNO, DB_CREATE | DB_TRUNCATE, 0644); // Erases anything already there
+    //user-input loop for SQL
+    std::string input = "";
+	cout << "SQL> \n";
+	getline(cin, input);
 
-	char block[BLOCK_SZ];
-	Dbt data(block, sizeof(block));
-	int block_number;
-	Dbt key(&block_number, sizeof(block_number));
-	block_number = 1;
-	strcpy(block, "hello!");
-	db.put(NULL, &key, &data, 0);  // write block #1 to the database
+	while (input != EXIT) {
+		//TODO: parse/execute SQL
+		
+		cout << "SQL> \n";
+		getline(cin, input);
+	}
 
-	Dbt rdata;
-	db.get(NULL, &key, &rdata, 0); // read block #1 from the database
-	std::cout << "Read (block #" << block_number << "): '" << (char *)rdata.get_data() << "'";
-	std::cout << " (expect 'hello!')" << std::endl;
-
-	return EXIT_SUCCESS;
+	return 0;
 }
