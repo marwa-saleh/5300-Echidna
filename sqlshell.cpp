5258ee52a85c464d7dc0222e3c0bf2662052f591
Meryll Cruz <mcruz3@seattleu.edu>
2022-04-24 15:43:54 -0700

    testing

--- sqlshell.cpp
+++ sqlshell.cpp
@@ -282,7 +282,7 @@ int main(int argc, char *argv[]) {
         if (query == "quit")
             break;  
         if (query == "test") {
-            cout << "test_heap_storage: " << (test_heap_storage() ? "ok" : "failed") << endl;
+        //    cout << "test_heap_storage: " << (test_heap_storage() ? "ok" : "failed") << endl;
             continue;
         }
 
