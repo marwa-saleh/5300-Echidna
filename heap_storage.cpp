9c6c9f6a1cb5d643730a71d21d9ec4e3d891710d
Meryll Cruz <mcruz3@seattleu.edu>
2022-04-24 15:54:50 -0700

    testing

--- heap_storage.cpp
+++ heap_storage.cpp
@@ -17,45 +17,45 @@ using namespace std;
 typedef uint16_t u16;
 
 //from provided test_heap_storage.cpp
-bool test_heap_storage() {
-    ColumnNames column_names;
-    column_names.push_back("a");
-    column_names.push_back("b");
-    ColumnAttributes column_attributes;
-    ColumnAttribute ca(ColumnAttribute::INT);
-    column_attributes.push_back(ca);
-    ca.set_data_type(ColumnAttribute::TEXT);
-    column_attributes.push_back(ca);
-    HeapTable table1("_test_create_drop_cpp", column_names, column_attributes);
-    table1.create();
-    cout << "create ok" << endl;
-    table1.drop();  // drop makes the object unusable because of BerkeleyDB restriction -- maybe want to fix this some day
-    cout << "drop ok" << endl;
+// bool test_heap_storage() {
+//     ColumnNames column_names;
+//     column_names.push_back("a");
+//     column_names.push_back("b");
+//     ColumnAttributes column_attributes;
+//     ColumnAttribute ca(ColumnAttribute::INT);
+//     column_attributes.push_back(ca);
+//     ca.set_data_type(ColumnAttribute::TEXT);
+//     column_attributes.push_back(ca);
+//     HeapTable table1("_test_create_drop_cpp", column_names, column_attributes);
+//     table1.create();
+//     cout << "create ok" << endl;
+//     table1.drop();  // drop makes the object unusable because of BerkeleyDB restriction -- maybe want to fix this some day
+//     cout << "drop ok" << endl;
 
-    HeapTable table("_test_data_cpp", column_names, column_attributes);
-    table.create_if_not_exists();
-    cout << "create_if_not_exsts ok" << endl;
+//     HeapTable table("_test_data_cpp", column_names, column_attributes);
+//     table.create_if_not_exists();
+//     cout << "create_if_not_exsts ok" << endl;
 
-    ValueDict row;
-    row["a"] = Value(12);
-    row["b"] = Value("Hello!");
-    cout << "try insert" << endl;
-    table.insert(&row);
-    cout << "insert ok" << endl;
-    // Handles* handles = table.select();
-    // cout << "select ok " << handles->size() << endl;
-    // ValueDict *result = table.project((*handles)[0]);
-    // cout << "project ok" << endl;
-    // Value value = (*result)["a"];
-    // if (value.n != 12)
-    //     return false;
-    // value = (*result)["b"];
-    // if (value.s != "Hello!")
-    //     return false;
-    // table.drop();
+//     ValueDict row;
+//     row["a"] = Value(12);
+//     row["b"] = Value("Hello!");
+//     cout << "try insert" << endl;
+//     table.insert(&row);
+//     cout << "insert ok" << endl;
+//     // Handles* handles = table.select();
+//     // cout << "select ok " << handles->size() << endl;
+//     // ValueDict *result = table.project((*handles)[0]);
+//     // cout << "project ok" << endl;
+//     // Value value = (*result)["a"];
+//     // if (value.n != 12)
+//     //     return false;
+//     // value = (*result)["b"];
+//     // if (value.s != "Hello!")
+//     //     return false;
+//     // table.drop();
 
-    return true;
-}
+//     return true;
+// }
 
 
 //---------------Slotted Page---------------
