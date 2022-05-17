84195504a30fc28a003ce71b0ef25192bf1e6b83
FangshengXuJason <xufangsheng@seattleu.edu>
2022-05-16 17:10:35 -0700

    manual add updated files

---
+++ heap_storage.h
@@ -0,0 +1,14 @@
+/**
+ * @file heap_storage.h - Implementation of storage_engine with a heap file structure.
+ * SlottedPage: DbBlock
+ * HeapFile: DbFile
+ * HeapTable: DbRelation
+ *
+ * @author Kevin Lundeen
+ * @see "Seattle University, CPSC5300, Spring 2022"
+ */
+#pragma once
+#include "SlottedPage.h"
+#include "HeapFile.h"
+#include "HeapTable.h"
+
